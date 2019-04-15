a3983bfb7497b6c8e11664d84b4205192c26d321
bokwoon <bokwoon.c@gmail.com>
2019-04-15 15:43:51 +0800

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p test; cd test
+mkdir 1 2 3
+touch a.txt b.txt c.txt d.md e.md f.md
+echo "hehe" | tee a.txt b.txt c.txt d.md e.md f.md
+touch .gitignore && printf "*\n*/\n!.gitignore" >> .gitignore
+mv a.txt b.txt 1; mv c.txt d.md 2; mv e.md f.md 3
