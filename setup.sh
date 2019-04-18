753d853dcbd06325cc65f713613cb3a53cdc5585
bokwoon <bokwoon.c@gmail.com>
2019-04-18 17:52:44 +0800

    wow I cant believe it finally works

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ touch a.txt b.txt c.txt d.md e.md f.md
 echo "hehe" | tee a.txt b.txt c.txt d.md e.md f.md
 touch .gitignore && printf "*\n*/\n!.gitignore" >> .gitignore
 mv a.txt b.txt 1; mv c.txt d.md 2; mv e.md f.md 3
+cd ..
+cp -r "test" "best"
